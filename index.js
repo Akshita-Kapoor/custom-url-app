d8617ccff02fdf7ea37c19243866c1171edcc7e5
Akshita <ak.akshita21@gmail.com>
2024-11-24 19:36:42 -0700

    index file

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const path = require("path");
 const URL = require("./models/url");
 const { connectMongoDb } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const app = express();
 const PORT = 8001;
 
 //Connection
 connectMongoDb("mongodb://localhost:27017/shortUrl");
 
+// set view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+//middlewares
 app.use(express.json());
+app.use(express.urlencoded({extended: false})) // for form data
+
 app.use("/url", urlRoute);
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRoute);
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
