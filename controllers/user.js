84dbfbaf9ba64fabe835db53331e7693b79b492e
Akshita <ak.akshita21@gmail.com>
2024-11-27 18:05:39 -0700

    authentication -controllers, middleware, routes

---
+++ controllers/user.js
@@ -0,0 +1,32 @@
+const User = require("../models/user");
+const { v4: uuidv4 } = require("uuid");
+const { setUser } = require("../service/auth");
+
+async function handleUserSignUp(req, res) {
+  const { name, email, password } = req.body;
+  // we can do validations.
+
+  await User.create({
+    name,
+    email,
+    password,
+  });
+  return res.render("/");
+}
+
+async function handleUserLogin(req, res) {
+  const { email, password } = req.body;
+  // we can do validations.
+
+  // user authentication
+  const user = await User.findOne({ email, password });
+  if (!user)
+    return res.render("login", {
+      error: "Invalid username or password",
+    });
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie('uid', sessionId);
+  return res.redirect("/");
+}
+module.exports = { handleUserSignUp, handleUserLogin };
